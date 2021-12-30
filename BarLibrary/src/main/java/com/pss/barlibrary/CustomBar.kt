11aa0bfff20b4105f10f383773b6aedcff8da350
ParkSangSun <appleasz1028102@gmail.com>
2021-12-30 10:37:03 +0900

    Add : ContrastBar

--- BarLibrary/src/main/java/com/pss/barlibrary/CustomBar.kt
+++ BarLibrary/src/main/java/com/pss/barlibrary/CustomBar.kt
@@ -41,5 +41,27 @@ class CustomBar {
             else winParams.flags = winParams.flags and lp.inv()
             win.attributes = winParams
         }
+
+
+        /**
+         * Display the icon of the notification bar and the background in a contrasting color
+         * 알림 bar의 아이콘과 백그라운드를 대비되는 색으로 표시
+         */
+        fun setContrastBar(a: Activity, color: Int) {
+            var flags = a.window?.decorView?.systemUiVisibility
+            if (flags != null) {
+                if (checkColor(color)) {
+                    flags = flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                    a.window?.decorView?.systemUiVisibility = flags
+                } else {
+                    flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    a.window?.decorView?.systemUiVisibility = flags
+                }
+            }
+            a.window?.statusBarColor = color
+        }
+
+        private fun checkColor(color: Int) =
+            1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255 >= 0.5
     }
 }
