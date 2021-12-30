f8060548a2cb1ba3cb85a33d50ca2d6ca412f166
ParkSangSun <appleasz1028102@gmail.com>
2021-12-30 11:09:38 +0900

    Add : Test

--- app/src/main/java/com/pss/notificationbarcustom/MainActivity.kt
+++ app/src/main/java/com/pss/notificationbarcustom/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.pss.notificationbarcustom
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pss.barlibrary.CustomBar.Companion.setContrastBar
+import com.pss.barlibrary.CustomBar.Companion.setTransparentBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTransparentBar(this)
     }
 }
